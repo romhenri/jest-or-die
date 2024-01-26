9a781aa5d3653b515095619a951914e3cca2b3ed
Zoiudico <toni.antoniocarlos2@gmail.com>
2024-01-26 20:29:34 -0300

    add: MainMenu
    add: TMP

---
+++ Assets/Game/Scripts/MenuBehavior.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MenuBehavior : MonoBehaviour
+{
+    public string cena;
+    public string cena2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+     
+    }
+
+    public void Options()
+    {
+        SceneManager.LoadScene(cena2); //Deletar ao iplementar opções
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(cena2);
+    }
+
+    public void QuitGame()
+    {
+        //No Editor use a linha de código abaixo e comente a outra
+        UnityEditor.EditorApplication.isPlaying = false;
+        //quando o jogo for compilado use a linha de código abaixo e comente a outra
+        //Application.Quit();
+    }
+
+}
