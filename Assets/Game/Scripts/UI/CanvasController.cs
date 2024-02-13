2b2466ecd86ced39f0cc5a7721b9d217ec77a05e
Rômulo Henri <romulohenri000@gmail.com>
2024-02-13 12:42:15 -0300

    add: Pause System

--- Assets/Game/Scripts/UI/CanvasController.cs
+++ Assets/Game/Scripts/UI/CanvasController.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CanvasController : MonoBehaviour
 {
     public GameObject WinScreen;
+    public GameObject PauseScreen;
 
     void Start()
     {
@@ -12,6 +14,34 @@ public class CanvasController : MonoBehaviour
 
     void Update()
     {
+        // Toggle pause screen
+        if (Input.GetKeyDown(KeyCode.Escape) || Input.GetKeyDown(KeyCode.P))
+        {
+            if (PauseScreen.activeSelf)
+            {
+                if (WinScreen.activeSelf)
+                {
+                    return;
+                }
+
+                UnsetPauseScreen();
+            }
+            else
+            {
+                if (WinScreen.activeSelf)
+                {
+                    return;
+                }
+
+                SetPauseScreen();
+            }
+        }
+
+        // Continue after win screen (Space or Enter)
+        if (WinScreen.activeSelf && (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.Return)))
+        {
+            SceneManager.LoadScene("MainMenu");
+        }
     }
 
     public void SetWinScreen()
@@ -30,5 +60,19 @@ public class CanvasController : MonoBehaviour
         {
             Destroy(knife);
         }
+    }
+
+    public void SetPauseScreen()
+    {
+        PauseScreen.SetActive(true);
+        Time.timeScale = 0;
+        GameObject.FindGameObjectWithTag("MainCamera").GetComponent<AudioSource>().Pause();
+    }
+
+    public void UnsetPauseScreen()
+    {
+        PauseScreen.SetActive(false);
+        Time.timeScale = 1;
+        GameObject.FindGameObjectWithTag("MainCamera").GetComponent<AudioSource>().Play();
     }
 }
