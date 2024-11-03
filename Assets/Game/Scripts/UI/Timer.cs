eaca98d03e96473d5660ad7c72549dab4589c1fb
Rômulo Henri <romulohenri000@gmail.com>
2024-11-02 23:39:02 -0300

    feat (Timer.cs): Script

---
+++ Assets/Game/Scripts/UI/Timer.cs
@@ -0,0 +1,58 @@
+using UnityEngine;
+using TMPro;
+
+public class Timer : MonoBehaviour
+{
+    public static Timer instance;
+    public TextMeshProUGUI timerText;
+    private float currentTime;
+    private bool isRunning;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void Update()
+    {
+        if (isRunning)
+        {
+            currentTime += Time.deltaTime;
+            UpdateTimerText();
+        }
+    }
+
+    public void StartTimer()
+    {
+        currentTime = 0;
+        isRunning = true;
+    }
+
+    public void StopTimer()
+    {
+        isRunning = false;
+    }
+
+    public void ResetTimer()
+    {
+        currentTime = 0;
+        isRunning = true;
+    }
+
+    private void UpdateTimerText()
+    {
+        int minutes = Mathf.FloorToInt(currentTime / 60);
+        int seconds = Mathf.FloorToInt(currentTime % 60);
+        int centiseconds = Mathf.FloorToInt((currentTime % 1) * 100);
+
+        timerText.text = string.Format("{0:00}:{1:00}:{2:00}", minutes, seconds, centiseconds);
+    }
+}
