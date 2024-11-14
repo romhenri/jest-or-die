d151d7e46f2378e1122cd128c05273c46300be95
Rômulo Henri <romulohenri000@gmail.com>
2024-11-13 22:48:53 -0300

    improve (Vase1.cs): Vase Break Sound

--- Assets/Game/Scripts/Active/Vase1.cs
+++ Assets/Game/Scripts/Active/Vase1.cs
@@ -6,18 +6,31 @@ public class Vase : MonoBehaviour
 {
     private Animator animator;
     private Collider2D collider;
+    private AudioSource audioSource;
+
+    [SerializeField] private GameObject coinPrefab;
+    [SerializeField] private GameObject bombPrefab;
+
+    [Range(0, 1)][SerializeField] private float coinChance = 0.5f;
+    [Range(0, 1)][SerializeField] private float bombChance = 0.2f;
 
     void Start()
     {
         animator = GetComponent<Animator>();
         collider = GetComponent<Collider2D>();
+        audioSource = GetComponent<AudioSource>();
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        TriggerBreakAnimation();
-
-        collider.enabled = false;
+        if (
+           collision.gameObject.CompareTag("Player") ||
+           collision.gameObject.CompareTag("Knife")
+        )
+        {
+            TriggerBreakAnimation();
+            collider.enabled = false;
+        }
     }
 
     void TriggerBreakAnimation()
@@ -28,11 +41,32 @@ public class Vase : MonoBehaviour
             rb.simulated = false;
         }
 
+        if (audioSource != null)
+        {
+            audioSource.Play();
+        }
+
         animator.SetTrigger("Break");
     }
 
     public void OnBreakAnimationEnd()
     {
-        Destroy(gameObject);
+        DropItem();
+        gameObject.SetActive(false);
+        //Destroy(gameObject);
+    }
+
+    void DropItem()
+    {
+        float randomValue = Random.value;
+
+        if (randomValue <= coinChance)
+        {
+            Instantiate(coinPrefab, transform.position, Quaternion.identity);
+        }
+        else if (randomValue <= coinChance + bombChance && bombPrefab != null)
+        {
+            Instantiate(bombPrefab, transform.position, Quaternion.identity);
+        }
     }
 }
