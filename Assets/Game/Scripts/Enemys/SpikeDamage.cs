79f961ee5a1d3030f8d32ba5c6780b26414d2e76
Rômulo Henri <romulohenri000@gmail.com>
2024-11-15 00:05:21 -0300

    feat (QuadSpikes.prefab): Multiple Spikes n Vases Prefabs

--- Assets/Game/Scripts/Enemys/SpikeDamage.cs
+++ Assets/Game/Scripts/Enemys/SpikeDamage.cs
@@ -2,21 +2,20 @@ using UnityEngine;
 
 public class SpikeDamage : MonoBehaviour
 {
-    public int damage = 1; // Quantidade de dano que o espinho causa (ajuste conforme necessário)
+    public int damage = 1;
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        // Verifica se o objeto que colidiu é o jogador, comparando a tag
         if (other.CompareTag("Player"))
         {
-            // Obtém o componente PlayerController do jogador
             var playerController = other.GetComponent<PlayerController>();
 
-            // Se o jogador tem o componente PlayerController, aplica o dano
             if (playerController != null)
             {
-                // Chama a função ReceiveDamage no jogador
                 playerController.ReceiveDamage();
+
+                Vector2 contactPoint = transform.position;
+                playerController.SpawnBlood(contactPoint);
             }
         }
     }
